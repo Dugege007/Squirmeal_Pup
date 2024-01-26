8871480ae290c13de76fc069f6e2e266d3741e47
Dugege007 <351080175@qq.com>
2024-01-26 21:44:37 +0800

    修复QF

---
+++ Assets/Scripts/Game/Player/HarpSealPup.Designer.cs
@@ -0,0 +1,10 @@
+// Generate Id:916783b3-b1ac-4423-bd75-e8c87d1d668b
+using UnityEngine;
+
+namespace SquirmealPup
+{
+	public partial class HarpSealPup
+	{
+
+	}
+}
---
+++ Assets/Scripts/Game/Player/HarpSealPup.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using QFramework;
+
+namespace SquirmealPup
+{
+	public partial class HarpSealPup : ViewController
+	{
+
+	}
+}
