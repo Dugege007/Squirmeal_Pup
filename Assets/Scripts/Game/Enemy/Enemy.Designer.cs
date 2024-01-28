e2824d88a19b6c7aca3f854aa120100912c8f17d
Dugege007 <351080175@qq.com>
2024-01-28 15:31:57 +0800

    准备其他动物、物品的素材；整理场景

---
+++ Assets/Scripts/Game/Enemy/Enemy.Designer.cs
@@ -0,0 +1,10 @@
+// Generate Id:61354348-6b5b-4c33-b95a-da2c6911a8c4
+using UnityEngine;
+
+namespace SquirmealPup
+{
+	public partial class Enemy
+	{
+
+	}
+}
---
+++ Assets/Scripts/Game/Enemy/Enemy.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using QFramework;
+
+namespace SquirmealPup
+{
+	public partial class Enemy : ViewController
+	{
+		void Start()
+		{
+			// Code Here
+		}
+	}
+}
